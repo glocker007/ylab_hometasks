389e05c71fdf27bf8fbbf02c447e5772e4cb8aae
glocker007 <glocker007@mail.ru>
2021-10-17 20:24:10 +0400

    you can choose start

--- dz2/hard_version/init.py
+++ dz2/hard_version/init.py
@@ -4,5 +4,10 @@ from agents import AgentClient, AgentGreedy
 agent_1 = AgentClient()
 agent_2 = AgentGreedy()
 agent_3 = AgentGreedy()
-game = Game(agent_2, agent_1)
+lst = [agent_2, agent_1]
+print("choose a sign [X/O]: ")
+s = input()
+if s == "X":
+    lst[0], lst[1] = lst[1], lst[0]
+game = Game(*lst)
 game.start_game()
